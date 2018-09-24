398838df95caf9fe362a4b66475689cd9dffcf56
Pri Nonis <priyankara.nonis@berkeley.edu>
2018-09-23 19:29:24 -0700

    Getting there

--- install.R
+++ install.R
@@ -1,6 +1,19 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages("leaflet")
-install.packages("ggplot2")
+import <- function(package)
+{
+  print(paste( 'Importing Package :', package))
+  if (!require(package, character.only = T))
+    install.packages(package, dep = T)
+  library(package, character.only = T)
+}
+
+import('tidyverse')
+import('ggthemes')
+import('repr')
+import('corrplot')
+import('psych')
+import('visNetwork')
+import('reshape2')
+import('showtext')
+
+font.add.google('Gloria Hallelujah', 'gloria')
+showtext.auto()
